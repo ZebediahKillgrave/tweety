b66629eaa0bad1739b8e926bf490b30ba7a7b145
Florent Espanet <florent.esp@gmail.com>
2013-12-18 13:03:52 +0100

    updated gitignore & added a todo header to the main.py file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+TODO:
+ - Use OAuth to connect to twitter API - done
+ - Find every mentions                 - done
+ - Store :                             - todo
+    * Tweet                            - done
+    * Author                           - done
+    * Hashtags                         - done
+    * Date & time                      - todo
+    * Retweet number                   - done
+    * List of retweeters               - done
+
+"""
+
 import sys
 try:
     import keys
@@ -6,6 +20,13 @@ except ImportError:
 You must follow the How To in README.md"""
     exit(1)
 import tweepy
+import sqlitedict
+
+db = sqlitedict.SqliteDict('tweepy.db', autocommit=True)
+try:
+    last_id = db['last_id']
+except KeyError:
+    db['last_id'] = 0
 
 auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
@@ -15,7 +36,9 @@ api = tweepy.API(auth)
 print api.me().name
 
 mentions = api.mentions_timeline()
+# mentions.reverse()
 
 print "   # MENTIONS"
 for mention in mentions:
-    print "%s: \"%s\"" % (mention.user.screen_name, mention.text)
+    retweets = mention.retweets()
+    print "%s: \"%s\" RT : %s, hastags : %s" % (mention.user.screen_name, mention.text, [rt.user.screen_name for rt in retweets], [word for word in mention.text.split() if word[0] == '#' and len(word) > 1])
