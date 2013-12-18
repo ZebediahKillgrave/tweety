076256d56a96088f36687b8853fb493fb14b017f
Florent Espanet <florent.esp@gmail.com>
2013-12-18 12:00:55 +0100

    the app connects to twitter, prints the current user & prints all the user mentions

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import sys
+try:
+    import keys
+except ImportError:
+    print >> sys.stderr, """Error : Could not import keys module.
+You must follow the How To in README.md"""
+    exit(1)
 import tweepy
 
-public_tweets = tweepy.api.public_timeline()
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-for tweet in public_tweets:
-    print tweet.text
+api = tweepy.API(auth)
+
+print api.me().name
+
+mentions = api.mentions_timeline()
+
+print "   # MENTIONS"
+for mention in mentions:
+    print "%s: \"%s\"" % (mention.user.screen_name, mention.text)
