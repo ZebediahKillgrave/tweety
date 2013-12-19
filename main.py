68237be7e5412039dc38e8d6a1fb1815686b650e
Florent Espanet <florent.esp@gmail.com>
2013-12-19 10:47:02 +0100

    switched to OO so it looks cleaner

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-"""
-TODO:
- - Use OAuth to connect to twitter API - done
- - Find every mentions                 - done
- - Store :                             - done
-    * Tweet                            - done
-    * Author                           - done
-    * Hashtags                         - done
-    * Date & time                      - done
-    * Retweet number                   - done
-    * List of retweeters               - done
-"""
-
 import sys
 try:
     import keys
@@ -21,39 +8,78 @@ You must follow the How To in README.md"""
 import tweepy
 import sqlitedict
 
-db = sqlitedict.SqliteDict('tweepy.db', autocommit=True)
-try:
-    last_id = db['last_id']
-except KeyError:
-    db['last_id'] = 0
+class Database(object):
+    def __init__(self, filename, autocommit = True):
+        self.db = sqlitedict.SqliteDict(filename, autocommit)
+        try:
+            self.db["last_id"]
+        except KeyError:
+            self.db["last_id"] = 0
 
-auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
-auth.set_access_token(keys.access_token, keys.access_token_secret)
+class Tweet(object):
+    def __init__(self, tweet):
+        self.id = tweet.id
+        self.author = tweet.user.screen_name
+        self.tweet = tweet.text
+        self.retweets = tweet.retweets()
+        self.rt_authors = [rt.user.screen_name for rt in self.retweets]
+        self.date = tweet.created_at
+        self.hashtags = [word for word in tweet.text.split() if self.word_is_hashtag(word)]
 
-api = tweepy.API(auth)
+    def word_is_hashtag(self, word):
+        return (word[0] == '#' and len(word) > 1)
 
-print api.me().name
-
-mentions = api.mentions_timeline()
-mentions.reverse()
-
-def word_is_hashtag(word):
-    return (word[0] == '#' and len(word) > 0)
-
-for mention in mentions:
-    try:
-        retweets = mention.retweets()
-    except tweepy.error.TweepError as e:
-        print >> sys.stderr, "Error %d : %s" % (e[0][0]["code"], e[0][0]["message"])
-        
-    if mention.id > db["last_id"]:
-        db["last_id"] = mention.id
-        db[mention.id] = {
-            "author"        : mention.user.screen_name,
-            "tweet"         : mention.text,
-            "retweets"      : len(retweets),
-            "rt_authors"    : [rt.user.screen_name for rt in retweets],
-            "hashtags"      : [word for word in mention.text.split() if word_is_hashtag(word)],
-            "date"          : mention.created_at
+    def archive(self, db):
+        db[self.id] = {
+            "author"            : self.author,
+            "tweet"             : self.tweet,
+            "retweets"          : len(self.retweets),
+            "rt_authors"        : self.rt_authors,
+            "date"              : self.date,
+            "hashtags"          : self.hashtags
             }
-        print "Added : %s" % (db[mention.id])
+
+    def __str__(self):
+        return "[%s] %s : %s\n(RT : %s (%d), HT : %s)" % (self.date, self.author, self.tweet,
+                                                          self.rt_authors, len(self.retweets),
+                                                          self.hashtags)
+
+class AuthHandler(object):
+    def __init__(self, consumer, consumer_secret, token, token_secret):
+        self.auth = tweepy.OAuthHandler(consumer, consumer_secret)
+        self.auth.set_access_token(token, token_secret)
+        self.api = tweepy.API(self.auth)
+
+class MentionManager(object):
+    def __init__(self, api):
+        self.mentions = api.mentions_timeline()
+        self.mentions.reverse()
+        self.tweets = []
+
+    def get_new_mentions(self, last_id):
+        for mention in self.mentions:
+            if mention.id > last_id:
+                try:
+                    self.tweets.append(Tweet(mention))
+                except tweepy.error.TweepError as e:
+                    print >> sys.stderr, "Error %d : %s" % (e[0][0]["code"], e[0][0]["message"])
+
+    def archive_mentions(self, db):
+        for tweet in self.tweets:
+            tweet.archive(db)
+
+def main():
+    db = Database("tweepy.db")
+    auth = AuthHandler(keys.consumer_key, keys.consumer_secret,
+                       keys.access_token, keys.access_token_secret)
+    engine = MentionManager(auth.api)
+    try:
+        db.db["last_id"]
+    except KeyError:
+        db.db["last_id"] = 0
+    finally:
+        engine.get_new_mentions(db.db["last_id"])
+    engine.archive_mentions(db.db)
+
+if __name__ == "__main__":
+    main()
